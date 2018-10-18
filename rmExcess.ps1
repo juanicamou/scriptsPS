13472baf35909e565590f6b4f928aaa3966519c2
Juan Ignacio Camou <juanignaciocamou@gmail.com>
2018-10-18 11:34:15 -0300

    Rename rmExcedente.ps1 to rmExcess.ps1

---
+++ rmExcess.ps1
@@ -0,0 +1,20 @@
+﻿$dirPath="D:\ANSI\IMG\Lote_23\"
+foreach ($dir in get-ChildItem $dirPath){
+$dir2 = $dirPath+$dir
+foreach ($file in get-ChildItem $dir2 *.pdf) {
+$a =  $dir2+"\"+$file
+
+
+$name =  Split-path -Path $a -leaf -Resolve
+$namexml = $name.replace('.pdf','.xml')
+$e = $dir2+"\"+ $namexml
+
+if(!(Test-Path -Path $e )){
+    $r = $dir2+"\"+ $name
+    Remove-Item $r 
+    echo "Eliminado:" $r
+}
+}
+} 
+
+echo "Listo!"
