f3272e5b0206e2cb0769fa0f77cfb70134f5ce68
Juan Ignacio Camou <juanignaciocamou@gmail.com>
2018-10-18 11:28:44 -0300

    Update and rename editImagePath.ps1 to EditImagePathXML.ps1

---
+++ EditImagePathXML.ps1
@@ -0,0 +1,14 @@
+﻿$dirPath="<<dir path>>"
+
+foreach ($file in get-ChildItem $dirPath *.xml -recurse) {
+		#Guardamos el contenido de nuestro fichero .XML en una variable
+		[xml]$XML = Get-Content $dirPath$file
+		#Modificamos el campo NOMBRE con el valor de la variable antes creada
+		$n = $XML.Acta.imagefilename
+        	$value = "71105"
+		$a = $value + $n
+		$XML.Acta.imagefilename = $a
+		#Guardamos el fichero .XML con la modificaci�n realizada
+		$savePath = "$dirPath$file"
+		$xml.save($savePath)
+	}
