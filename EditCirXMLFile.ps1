4376d6cffeab7a9fc3c08441b61ede36337caa45
Juan Ignacio Camou <juanignaciocamou@gmail.com>
2018-10-18 11:26:49 -0300

    Update and rename editCIR.ps1 to EditCirXMLFile.ps1

--- EditCirXMLFile.ps1
+++ EditCirXMLFile.ps1
@@ -1,11 +1,8 @@
-﻿$dirPath="C:\Users\Usuario\Desktop\Para Juanito\36822\"
+﻿$dirPath="<<dir path>>"
 
 foreach ($file in get-ChildItem $dirPath *.xml -recurse) {
 		#Guardamos el contenido de nuestro fichero .XML en una variable
 		[xml]$XML = Get-Content $dirPath$file
-		#$file -replace "<fechadenacimiento>", "<fechanacimiento>"
-		#Creamos una variable para almacenar el valor a cambiare
-		#$value1= "PRUEBA"
 
 		#Modificamos el campo NOMBRE con el valor de la variable antes creada
 		#$n = $XML.Acta.circunscripcion
@@ -14,7 +11,4 @@ foreach ($file in get-ChildItem $dirPath *.xml -recurse) {
 		#Guardamos el fichero .XML con la modificaci�n realizada
 		$savePath = "$dirPath$file"
 		$xml.save($savePath)
-
-		#(Get-Content $dirPath$file).replace("<fechadenacimiento>", "<fechanacimiento>") | Set-Content $dirPath$file
-		#(Get-Content $dirPath$file).replace("</fechadenacimiento>", "</fechanacimiento>") | Set-Content $dirPath$file
 	}
